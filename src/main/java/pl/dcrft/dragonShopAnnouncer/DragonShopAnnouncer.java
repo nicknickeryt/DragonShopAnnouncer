79d60c820e478a492cd568bff53b7949fa34459c
Nick Nicker <nick4nicker@gmail.com>
2025-12-28 00:31:21 +0100

    initial plugin

--- src/main/java/pl/dcrft/dragonShopAnnouncer/DragonShopAnnouncer.java
+++ src/main/java/pl/dcrft/dragonShopAnnouncer/DragonShopAnnouncer.java
@@ -1,18 +1,43 @@
 package pl.dcrft.dragonShopAnnouncer;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.command.CommandManager;
+import com.velocitypowered.api.command.CommandMeta;
+import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
 import org.slf4j.Logger;
 
 @Plugin(id = "dragonshopannouncer", name = "DragonShopAnnouncer", version = "1.0-SNAPSHOT", description = "Stats manager for DragonCraft", url = "DCRFT.PL", authors = {"NickNickerYT"})
 public class DragonShopAnnouncer {
+    private final ProxyServer proxy;
+    private static DragonShopAnnouncer instance;
+
+    public ProxyServer getProxy() {
+        return proxy;
+    }
+
+    public static DragonShopAnnouncer getInstance() {
+        return instance;
+    }
 
     @Inject
-    private Logger logger;
+    public DragonShopAnnouncer(ProxyServer proxy) {
+        this.proxy = proxy;
+        instance = this;
+    }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+        CommandManager commandManager = proxy.getCommandManager();
+        CommandMeta commandMeta = commandManager.metaBuilder("shopannounce")
+                .plugin(this)
+                .build();
+
+        SimpleCommand commandToRegister = new ShopAnnounceCommand();
+
+        commandManager.register(commandMeta, commandToRegister);
     }
 }
--- src/main/java/pl/dcrft/dragonShopAnnouncer/ShopAnnounceCommand.java
+++ src/main/java/pl/dcrft/dragonShopAnnouncer/ShopAnnounceCommand.java
@@ -1,4 +1,37 @@
 package pl.dcrft.dragonShopAnnouncer;
 
-public class ShopAnnounceCommand {
+import com.velocitypowered.api.command.SimpleCommand;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+public class ShopAnnounceCommand implements SimpleCommand {
+    private static String prefix = "&e&lDragon&6&lCraft &e» ";
+
+    @Override
+    public void execute(final Invocation invocation) {
+        String[] args = invocation.arguments();
+
+        if(args.length < 2) return;
+
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < args.length; ++i) sb.append(args[i]).append(" ");
+        final String allArgs = sb.toString().trim();
+
+        String player = args[0];
+        String item = allArgs;
+
+        Component message = LegacyComponentSerializer.legacyAmpersand().deserialize(
+                prefix + "&3Gracz &b" + player + " &3zakupił &b" + item + "&3!" + "\n"
+                + "&e» &3Dziękujemy za wsparcie!" + "\n"
+                + "&e» &6Smocze jaja &bdostępne od &61zł&b!" + "\n"
+                + "&e» &bSprawdź ofertę na &6DCRFT.PL &c❤"
+        );
+
+        DragonShopAnnouncer.getInstance().getProxy().sendMessage(message);
+    }
+
+    public boolean hasPermission(final Invocation invocation) {
+        return invocation.source().hasPermission("dcshop.use");
+    }
+
 }
